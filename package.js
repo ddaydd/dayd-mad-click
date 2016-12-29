c15792b9cceb3e6749a9f3b87634e129b17151a9
David FAIVRE-MAÇON <faivred@gmail.com>
2016-12-29 15:01:23 +0100

    fix memory bug, v0.0.4

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'dayd:mad-click',
-  version: '0.0.3',
+  version: '0.0.4',
   // Brief, one-line summary of the package.
   summary: "Mad Click",
   // URL to the Git repository containing the source code for this package.
@@ -11,7 +11,7 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.1.0.3');
+  api.versionsFrom('1.4.2.3');
 
   api.use([
     'mongo',
